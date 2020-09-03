f90c9b7b9c731e86f7e7f33b55d00ad7a62a3e8a
twcheung36 <twcheung36@users.noreply.github.com>
2020-09-02 22:31:15 -0400

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,22 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://www.linkedin.com/in/twcheung/" target="_blank">
+          Wing Cheung
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/twcheung36/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
