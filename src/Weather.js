09fc3bef69e8b5784e828ca5a5d430bd9255a5f3
twcheung36 <twcheung36@users.noreply.github.com>
2020-09-06 21:42:16 -0400

    Use the correct icon URL

--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,7 @@ export default function Weather(props) {
       date: new Date(response.data.dt * 1000),
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
-      iconURL: "http://openweathermap.org/img/wn/10d@2x.png",
+      iconURL: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       wind: response.data.wind.speed,
       city: response.data.name,
       pressure: response.data.main.pressure,
